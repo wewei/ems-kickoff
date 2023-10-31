826f1181c12ab51e3dec031472c10082dc1086af
Scott Wei <leescott@microsoft.com>
2023-10-31 13:18:01 +0800

    Add the lottery assets

--- rspack.config.ts
+++ rspack.config.ts
@@ -3,16 +3,27 @@ import _ from 'lodash';
 import { HtmlRspackPlugin } from '@rspack/core';
 
 const entry = {
-    lottery: './app/lottery.ts',
-    register: './app/register.tsx',
+    lottery: './app/lottery/index.js',
+    register: './app/register/index.tsx',
 };
 
 const config: Configuration = {
     entry,
+    builtins: {
+        copy: {
+            patterns: [{
+                from: './app/lottery/lib',
+                to: './lottery/lib',
+            }, {
+                from: './app/lottery/assets',
+                to: './lottery/assets',
+            }],
+        },
+    },
     plugins: [
         ..._.keys(entry).map(ent => new HtmlRspackPlugin({
-            template: `./app/public/${ent}.html`,
-            filename: `./${ent}.html`,
+            template: `./app/${ent}/index.html`,
+            filename: `./${ent}/index.html`,
             chunks: [ent],
             minify: true,
             inject: true,
